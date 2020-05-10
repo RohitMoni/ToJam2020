5706466a099c318df8443e67466b41af7e574e89
Rohit Moni <rohitmdxb@gmail.com>
2020-05-10 18:19:25 -0400

    Setting up seating scene to use global guest data

---
+++ Assets/Code/SeatingSceneScripts/SSGuestListManager.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SSGuestListManager : MonoBehaviour
+{
+    public GameObject[] guestList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setup Guest list with global generated
+        var dinnerPartyGlobals = GameObject.Find("Globals").GetComponent<DinnerPartyGlobals>();
+
+        for (int i = 0; i < guestList.Length; ++i)
+        {
+            var portraitObject = guestList[i].transform.Find("Portrait");
+            var nameObject = guestList[i].transform.Find("Name");
+
+            // Set up portrait object to be replaced by our guest object
+            // Set up the guests transform to be the same as the old portrait transform
+            dinnerPartyGlobals.Guests[i].transform.SetParent(portraitObject.transform);
+            dinnerPartyGlobals.Guests[i].transform.localPosition = Vector3.zero;
+            dinnerPartyGlobals.Guests[i].transform.localRotation = Quaternion.identity;
+            dinnerPartyGlobals.Guests[i].transform.localScale = Vector3.zero;
+
+            // Move the guest to be a sibling of the old guest item
+            dinnerPartyGlobals.Guests[i].transform.SetParent(guestList[i].transform);
+
+            // Delete the old portrait object
+            Destroy(portraitObject);
+
+            // We don't need to replace the entire name object, instead just change the text
+            nameObject.GetComponent<TextMeshProUGUI>().text = dinnerPartyGlobals.Persons[i].name;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
